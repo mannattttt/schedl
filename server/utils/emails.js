4e45f10bb7aaad35f16c4b4d9ef7a23a4edec8f8
Mannat Chaudhary <140438710+mannattttt@users.noreply.github.com>
2026-03-13 14:24:56 +0530

    update nodemailer port to 587 for Railway

--- server/utils/emails.js
+++ server/utils/emails.js
@@ -7,7 +7,9 @@ if (require('dns').setDefaultResultOrder) {
 }
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
